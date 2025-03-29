02a1b8dcea7ef6ce901dae9868c3d8bc17d8984e
Derlie Monne <derlie.monne@gmail.com>
2025-03-29 23:53:07 +1000

    main screen with player

--- song_guesser/lib/main.dart
+++ song_guesser/lib/main.dart
@@ -1,20 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(const MainApp());
 }
+
+class MainAppState extends ChangeNotifier {}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (context) => MainAppState(),
+      child: MaterialApp(title: "song guesser", home: Home()),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Player(), FormWidget()],
         ),
       ),
     );
+  }
+}
+
+class FormWidget extends StatefulWidget {
+  const FormWidget({super.key});
+
+  @override
+  State<FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<FormWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                constraints: BoxConstraints(maxWidth: 300),
+                child: TextFormField(
+                  decoration: const InputDecoration(hintText: 'your guess'),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Tooltip(
+                message:
+                    "enter artist and song. case insensitive. apostrophes are optional. you don't need to type ft/feat/live or something like this.",
+                triggerMode: TooltipTriggerMode.manual,
+                child: IconButton(
+                  icon: const Icon(Icons.info),
+                  onPressed: null,
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('guess!'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Player extends StatefulWidget {
+  const Player({super.key});
+
+  @override
+  State<Player> createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  Widget _playerButton(PlayerState playerState) {
+    final processingState = playerState.processingState;
+
+    if (processingState == ProcessingState.loading ||
+        processingState == ProcessingState.buffering) {
+      return Container(
+        margin: EdgeInsets.all(8.0),
+        width: 64.0,
+        height: 64.0,
+        child: CircularProgressIndicator(),
+      );
+    } else if (!_audioPlayer.playing) {
+      return IconButton(
+        icon: Icon(Icons.play_arrow),
+        iconSize: 64.0,
+        onPressed: _audioPlayer.play,
+      );
+    } else if (processingState != ProcessingState.completed) {
+      return IconButton(
+        icon: Icon(Icons.pause),
+        iconSize: 64.0,
+        onPressed: _audioPlayer.pause,
+      );
+    }
+    return Placeholder();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer
+        .setAudioSource(
+          ConcatenatingAudioSource(
+            children: [
+              AudioSource.uri(
+                Uri.parse(
+                  "https://archive.org/download/IGM-V7/IGM%20-%20Vol.%207/25%20Diablo%20-%20Tristram%20%28Blizzard%29.mp3",
+                ),
+              ),
+              AudioSource.uri(
+                Uri.parse(
+                  "https://archive.org/download/igm-v8_202101/IGM%20-%20Vol.%208/15%20Pokemon%20Red%20-%20Cerulean%20City%20%28Game%20Freak%29.mp3",
+                ),
+              ),
+              AudioSource.uri(
+                Uri.parse(
+                  "https://scummbar.com/mi2/MI1-CD/01%20-%20Opening%20Themes%20-%20Introduction.mp3",
+                ),
+              ),
+            ],
+          ),
+        )
+        .catchError((error) {
+          // catch load errors: 404, invalid url ...
+          print("An error occurred $error");
+        });
+    _audioPlayer.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+      stream: _audioPlayer.playerStateStream,
+      builder: (context, snapshot) {
+        final playerState = snapshot.data;
+        return _playerButton(playerState!);
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
   }
 }
